03c2923c57a5a469bb3a685d04f59dbf89911851
andre820 <andre820@gmail.com>
2014-11-01 19:22:51 -0200

    Improving Makefiles

--- opt/msim-rules.mk
+++ opt/msim-rules.mk
@@ -1,13 +1,18 @@
 
+ROOT_DIR ?= .
 $(LIB): $(ROOT_DIR)/$(LIB)/_info $(call vhd2obj,$(SOURCE_LIST))
 
 $(ROOT_DIR)/%/_info:
-	vlib $(ROOT_DIR)/$*
-	vmap -modelsimini $(ROOT_DIR)/modelsim.ini $* $(ROOT_DIR)/$*
-
-%/_info:
-	vlib $(ROOT_DIR)/$*
-	vmap $* $(ROOT_DIR)/$*
+	if [ ! -d "$(ROOT_DIR)" ]; then \
+		mkdir -p $(ROOT_DIR); \
+	fi
+	if [ -f "$(ROOT_DIR)/modelsim.ini" ]; then \
+		vlib $(ROOT_DIR)/$*; \
+		vmap -modelsimini $(ROOT_DIR)/modelsim.ini $* $(ROOT_DIR)/$*; \
+	else \
+		vlib $(ROOT_DIR)/$*; \
+		vmap $* $(ROOT_DIR)/$*; \
+	fi
 
 $(ROOT_DIR)/$(LIB)/%/_primary.dat: %.vhd
 	vcom -$(VHDL_VERSION) $(VCOM_ARGS_G) $(VCOM_ARGS) -modelsimini $(ROOT_DIR)/modelsim.ini -work $(ROOT_DIR)/$(LIB) $*.vhd
