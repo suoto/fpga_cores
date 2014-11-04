973fd63d7c7c7e5f6cc5bbd5d24689b3773fd460
andre820 <andre820@gmail.com>
2014-11-04 00:36:46 -0200

    Improved vhd compilation time

--- opt/msim-rules.mk
+++ opt/msim-rules.mk
@@ -1,7 +1,9 @@
 
 ROOT_DIR ?= .
 
-$(LIB): $(ROOT_DIR)/$(LIB)/_info $(call vhd2obj,$(SOURCE_LIST))
+FILES =
+
+$(LIB): $(ROOT_DIR)/$(LIB)/_info $(call vhd2obj,$(SOURCE_LIST)) comp-lib
 
 $(ROOT_DIR)/%/_info:
 	if [ -f "$(ROOT_DIR)/modelsim.ini" ]; then \
@@ -13,7 +15,13 @@ $(ROOT_DIR)/%/_info:
 	fi
 
 $(ROOT_DIR)/$(LIB)/%/_primary.dat: %.vhd
-	vcom -$(VHDL_VERSION) $(VCOM_ARGS_G) $(VCOM_ARGS) -modelsimini $(ROOT_DIR)/modelsim.ini -work $(ROOT_DIR)/$(LIB) $*.vhd
+	$(eval FILES += $*.vhd)
+
+comp-lib:
+	@if [ "$(FILES)" != "" ]; then \
+		echo vcom -$(VHDL_VERSION) $(VCOM_ARGS_G) $(VCOM_ARGS) -modelsimini $(ROOT_DIR)/modelsim.ini -work $(ROOT_DIR)/$(LIB) $(FILES); \
+		vcom -$(VHDL_VERSION) $(VCOM_ARGS_G) $(VCOM_ARGS) -modelsimini $(ROOT_DIR)/modelsim.ini -work $(ROOT_DIR)/$(LIB) $(FILES); \
+	fi
 
 clean-lib:
 	rm -rf $(ROOT_DIR)/$(LIB)/ modelsim.ini
