349dbbb7dd47125efc114017e3a5de5c9796fafb
suoto <andre820@gmail.com>
2016-07-16 00:32:22 -0300

    Removed Makefiles as test flow will use VUnit

--- opt/msim-options.mk
+++
@@ -1,7 +0,0 @@
-
-VHDL_VERSION ?= -93
-VCOM_ARGS_G ?= -64 -explicit -O5
-VSIM_ARGS_G ?= -novopt -64 -quiet
-
-#vhd2obj = $(patsubst %.vhd,$(ROOT_DIR)/$(LIB)/%/$&.dat,$(1))
-vhd2obj = $(patsubst %.vhd,$(ROOT_DIR)/$(LIB)/.db/%,$(1))
