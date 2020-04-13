6ea7be375f741d78bdfe401a8f17251a8dca8aa8
suoto <andre820@gmail.com>
2020-04-13 09:45:58 +0100

    Adding AXI stream package

---
+++ src/axi_pkg.vhd
@@ -0,0 +1,39 @@
+--
+-- FPGA Cores -- An HDL core library
+--
+-- Copyright 2020 by Andre Souto (suoto)
+--
+-- This file is part of FPGA Cores.
+-- 
+-- FPGA Cores is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+-- 
+-- FPGA Cores is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with FPGA Cores.  If not, see <http://www.gnu.org/licenses/>.
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+package axi_pkg is
+
+  type axi_stream_bus_t is record
+    tdata  : std_logic_vector;
+    tuser  : std_logic_vector;
+    tvalid : std_logic;
+    tready : std_logic;
+    tlast  : std_logic;
+  end record;
+
+  subtype axi_stream_data_bus_t is axi_stream_bus_t(tdata(open), tuser(-1 downto 0));
+
+end package axi_pkg;
+
+package body axi_pkg is
+end package body;
