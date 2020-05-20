2b53fece3c0359333dce0af8160a901f4305a818
suoto <andre820@gmail.com>
2020-05-20 21:04:57 +0100

    Fixing RAM inference delay instantiation

--- src/ram_inference.vhd
+++ src/ram_inference.vhd
@@ -58,6 +58,27 @@ end ram_inference;
 
 architecture ram_inference of ram_inference is
 
+  -- Assign block RAM if the RAM size is bigger than 150% of a 18KB block RAM
+  function get_ram_style return string is
+    constant BRAM_SIZE     : integer := 18 * 1024;
+    constant BRAM_TRESHOLD : real    := 1.5;
+
+    constant size : natural := (2**ADDR_WIDTH) * DATA_WIDTH;
+  begin
+    if RAM_TYPE /= "auto"  then
+      return RAM_TYPE;
+    end if;
+
+    if real(size / BRAM_SIZE) > BRAM_TRESHOLD then
+      return "block";
+    end if;
+
+    return "distributed";
+
+  end function get_ram_style;
+
+  constant RESOLVED_RAM_STYLE : string := get_ram_style;
+
   -----------
   -- Types --
   -----------
@@ -71,7 +92,7 @@ architecture ram_inference of ram_inference is
   signal rddata_b_i : std_logic_vector(DATA_WIDTH - 1 downto 0);
 
   attribute RAM_STYLE        : string;
-  attribute RAM_STYLE of ram : signal is RAM_TYPE;
+  attribute RAM_STYLE of ram : signal is RESOLVED_RAM_STYLE;
 
 begin
 
@@ -84,16 +105,49 @@ begin
   -------------------
   -- Port mappings --
   -------------------
-  rddata_a_delay : entity work.sr_delay
-    generic map (
-      DELAY_CYCLES => OUTPUT_DELAY,
-      DATA_WIDTH   => DATA_WIDTH)
-    port map (
-      clk     => clk_a,
-      clken   => clken_a,
+  gen_not_bram_delay : if RESOLVED_RAM_STYLE /= "block" generate
+    rddata_a_delay : entity work.sr_delay
+      generic map (
+        DELAY_CYCLES => OUTPUT_DELAY,
+        DATA_WIDTH   => DATA_WIDTH)
+      port map (
+        clk     => clk_a,
+        clken   => clken_a,
 
-      din     => rddata_a_i,
-      dout    => rddata_a);
+        din     => rddata_a_i,
+        dout    => rddata_a);
+
+    rddata_a_i <= ram(to_integer(unsigned(addr_a)));
+
+  end generate;
+
+  -- Need this workaround so that Vivado manages to infer a block RAM when specified
+  gen_bram_delay : if RESOLVED_RAM_STYLE = "block" generate
+    assert OUTPUT_DELAY /= 0
+      report "Can't use RAM_TYPE " & quote(RESOLVED_RAM_STYLE) & " with output delay set to " & integer'image(OUTPUT_DELAY)
+      severity Failure;
+
+    rddata_a_delay : entity work.sr_delay
+      generic map (
+        DELAY_CYCLES => OUTPUT_DELAY - 1,
+        DATA_WIDTH   => DATA_WIDTH)
+      port map (
+        clk     => clk_a,
+        clken   => clken_a,
+
+        din     => rddata_a_i,
+        dout    => rddata_a);
+
+    process(clk_a)
+    begin
+      if clk_a'event and clk_a = '1' then
+        if clken_a = '1' then
+          rddata_a_i <= ram(to_integer(unsigned(addr_a)));
+        end if;
+      end if;
+    end process;
+
+  end generate;
 
   rddata_b_delay : entity work.sr_delay
     generic map (
@@ -106,7 +160,6 @@ begin
       din     => rddata_b_i,
       dout    => rddata_b);
 
-  rddata_a_i <= ram(to_integer(unsigned(addr_a)));
   rddata_b_i <= ram(to_integer(unsigned(addr_b)));
 
   ---------------
