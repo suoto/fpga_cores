da55ac4208fc97c8b06bbaf4e69dc053b66622dd
suoto <andre820@gmail.com>
2026-06-19 18:47:43 +0100

    Increase axi_file_reader_tb watchdog timeout to 100 ms

--- testbench/axi_file_reader_tb.vhd
+++ testbench/axi_file_reader_tb.vhd
@@ -147,7 +147,7 @@ begin
   -- Asynchronous assignments --
   ------------------------------
   clk <= not clk after CLK_PERIOD/2;
-  test_runner_watchdog(runner, 10 ms);
+  test_runner_watchdog(runner, 100 ms);
 
   ---------------
   -- Processes --
